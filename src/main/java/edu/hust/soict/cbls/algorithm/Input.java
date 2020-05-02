dd9f03fe54b4372350acd1f7791e0a1441cf91db
dongocson98 <dongocson.work@gmail.com>
2020-05-02 21:36:25 +0700

    before-pull

--- src/main/java/edu/hust/soict/cbls/algorithm/Input.java
+++ src/main/java/edu/hust/soict/cbls/algorithm/Input.java
@@ -1,4 +1,15 @@
 package edu.hust.soict.cbls.algorithm;
 
 public class Input {
+
+    public Integer N;
+
+    public Integer M;
+
+    public Double[] q;
+
+    public Double[] Q;
+
+    public Double[][] d;
+
 }
