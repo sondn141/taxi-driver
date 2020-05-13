32b385627dbe58d389f122fefe63bdf40173174c
dongocson98 <dongocson.work@gmail.com>
2020-05-13 13:14:12 +0700

    before-pull

---
+++ src/main/java/edu/hust/soict/cbls/algorithm/nsgaii/MyProblem.java
@@ -0,0 +1,6 @@
+package edu.hust.soict.cbls.algorithm.nsgaii;
+
+public class MyProblem {
+
+
+}
--- src/main/java/edu/hust/soict/cbls/algorithm/nsgaii/NSGAIISolver.java
+++ src/main/java/edu/hust/soict/cbls/algorithm/nsgaii/NSGAIISolver.java
@@ -1,16 +1,13 @@
+package edu.hust.soict.cbls.algorithm.nsgaii;
+
 import edu.hust.soict.cbls.algorithm.Solution;
 import edu.hust.soict.cbls.algorithm.Solver;
 import edu.hust.soict.cbls.common.config.Properties;
 
-public class Test extends Solver {
+public class NSGAIISolver extends Solver {
 
-    public Test(Properties props) {
+    public NSGAIISolver(Properties props) {
         super(props);
-    }
-
-    public static void main(String[] args) {
-        Test test = new Test(new Properties());
-        System.out.println("");
     }
 
     @Override
