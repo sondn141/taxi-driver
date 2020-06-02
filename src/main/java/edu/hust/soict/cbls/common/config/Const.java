813a9c57be27e7b7571123a94c11c86aa78a6693
dongocson98 <dongocson.work@gmail.com>
2020-06-02 19:58:00 +0700

    pull-factor

--- src/main/java/edu/hust/soict/cbls/common/config/Const.java
+++ src/main/java/edu/hust/soict/cbls/common/config/Const.java
@@ -75,8 +75,8 @@ public class Const {
     public static final Color[] COLORS = new Color[]{
             new Color(0, 0, 0),
             new Color(255,0,0),
-            new Color(0, 255, 0),
             new Color(0, 0, 255),
+            new Color(0, 255, 0),
             new Color(255, 255, 0),
             new Color(0, 255, 255),
             new Color(255, 0, 255),
